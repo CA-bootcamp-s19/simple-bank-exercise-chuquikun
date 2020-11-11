18845011b9f8f894e646808c8cbab9ff3de97419
chuqui <fgarlag@gmail.com>
2020-11-10 19:33:56 -0600

    Define event arguments and added an event for withdrawals

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -19,20 +19,21 @@ contract SimpleBank {
     mapping (address => bool) public enrolled;
 
     /* Let's make sure everyone knows who owns the bank. Use the appropriate keyword for this*/
-    address public  owner;
+    address public owner;
     
     //
     // Events - publicize actions to external listeners
     //
     
     /* Add an argument for this event, an accountAddress */
-    event LogEnrolled();
+    event LogEnrolled(address indexed accountAddress);
 
     /* Add 2 arguments for this event, an accountAddress and an amount */
-    event LogDepositMade();
+    event LogDepositMade(address indexed accountAddress, uint amount);
 
     /* Create an event called LogWithdrawal */
     /* Add 3 arguments for this event, an accountAddress, withdrawAmount and a newBalance */
+    event LogWithdrawal(address indexed accountAddress, uint withdrawAmount,uint newBalance)
 
 
     //
@@ -42,6 +43,7 @@ contract SimpleBank {
     /* Use the appropriate global variable to get the sender of the transaction */
     constructor() public {
         /* Set the owner to the creator of this contract */
+        owner = msg.sender;
     }
 
     // Fallback function - Called if other functions don't match call or
